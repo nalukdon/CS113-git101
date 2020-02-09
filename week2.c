89cfadfce82e42ff0de912df960765cf126d63a3
PIP <naluedon.a@ku.th>
2020-02-09 14:00:31 +0700

    printf and data type

---
+++ week2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,c=1;
+    float d=1.5;
+    printf("Hello, world\n");
+    printf("6210450601\n");
+    printf("print(\"Hello, world\\n\");");
+}
+
+/*gcc .\[ชื่อไฟล์]
+ครั้งแรกจะได้ ชื่อไฟล์เป็น a.exe
+ถ้า gcc .\[ชื่อ] -o [ชื่อexe]
++= 1 ใช้ ++ ได้
+-= 1 ใช้ -- ได้
+/ มีอันเดียว*/
+
